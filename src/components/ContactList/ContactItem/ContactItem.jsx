2aaa0b6fecd8193113e117afde9cc438bc71d28b
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2023-01-03 19:20:54 +0100

    Update ContactItem.jsx

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,12 +1,7 @@
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
 import Button from '../RemoveButton/RemoveButton';
-import { getLoading } from 'redux/contacts/contactsSelector';
 
 export default function ContactItem({ name, number, id }) {
-  const loading = useSelector(getLoading);
-  console.log('🚀 ~ loading', loading);
-
   return (
     <>
       <div>
